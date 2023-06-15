4796984d5cf3f82910213cbaaefd5bc29680e4f3
Joshua Swan <thejoshuaswan@gmail.com>
2023-06-14 20:08:39 -0500

    declared and initialized the variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string catName = "Finn";
+            int age = 10;
+            char lastInitial = 'S';
+            bool isCute = true;
+            double length = 26.5;
+            decimal weight = 18.23m;
+
+            Console.WriteLine($"My cat's name is {catName}. He is {age} years old, and his last name starts with {lastInitial}." +
+                $" The fact that he's cute is definitely {isCute}, and his length and weight are " +
+                $"{length} inches and {weight} pounds respectively");
         }
     }
 }
